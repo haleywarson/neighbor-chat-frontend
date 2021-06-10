59693bcc4b2a4383ad41b2fc393bac80f905dac9
Haley Warson <haleywarson@gmail.com>
2021-06-09 20:41:16 -0600

    Adds fetch to pull data from backend.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+// import socketClient from "socket.io-client";
+// const SERVER = "http://127.0.0.1:8080";
+import "./App.css";
 
 function App() {
+  // var socket = socketClient(SERVER);
+  // socket.on("connection", () => {
+  //   console.log(`I'm connected with the back-end`);
+  // });
+
+  const [users, setUsers] = useState([]);
+
+  const fetchUsers = () => {
+    fetch("http://localhost:9000/users")
+      .then((response) => response.json())
+      .then((users) => setUsers(users));
+  };
+
+  const displayUsers = () => {
+    return users.map((user) => <li>{user.username}</li>);
+  };
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>{displayUsers()}</ul>
     </div>
   );
 }
